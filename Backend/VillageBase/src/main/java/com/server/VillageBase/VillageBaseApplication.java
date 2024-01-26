af1834522a4ecd944ec0d28668e0f64db33d88a0
egohommeli12345 <pirnes.samuli@gmail.com>
2024-01-26 10:32:05 +0200

    frontend and backend initialized

---
+++ Backend/VillageBase/src/main/java/com/server/VillageBase/VillageBaseApplication.java
@@ -0,0 +1,13 @@
+package com.server.VillageBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillageBaseApplication.class, args);
+	}
+
+}
