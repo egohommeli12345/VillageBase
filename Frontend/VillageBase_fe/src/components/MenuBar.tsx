54e95b9715e32ff842d45a2f0c060cb9ee94e1ee
Tam Phuong <Tam.Phuong@edu.savonia.fi>
2024-02-01 18:02:45 +0200

    Update MenuBar styling and functionality

--- Frontend/VillageBase_fe/src/components/MenuBar.tsx
+++ Frontend/VillageBase_fe/src/components/MenuBar.tsx
@@ -2,8 +2,24 @@ import "./MenuBar.css";
 
 export default function MenuBar() {
   return (
-    <>
-      <div className="menuBarBG">Minecraft</div>
-    </>
+    <div className="menuBarBG">
+      <div className="buttons">
+        <a className="button">Alueet ja mökit</a>
+        <a className="button">Palvelut</a>
+        <a className="button">Majoitusvaraukset</a>
+        <a className="button">Asiakkaat</a>
+        <a className="button">Laskutus</a>
+        <a className="button">Raportit</a>
+
+        <div className="search-container">
+          {" "}
+          {/* Lisätty tämä divi hakuelementeille */}
+          <input type="text" className="search-input" placeholder="Etsi" />
+          <button type="submit" className="search-button">
+            Etsi
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
