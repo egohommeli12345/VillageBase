79613fd4a63a896eb489601f91965c70633709d4
egohommeli12345 <pirnes.samuli@gmail.com>
2024-02-01 18:17:05 +0200

    button change on menubar

--- Frontend/VillageBase_fe/src/components/MenuBar.tsx
+++ Frontend/VillageBase_fe/src/components/MenuBar.tsx
@@ -1,25 +1,27 @@
 import "./MenuBar.css";
 
 export default function MenuBar() {
-  return (
-    <div className="menuBarBG">
-      <div className="buttons">
-        <a className="button">Alueet ja mökit</a>
-        <a className="button">Palvelut</a>
-        <a className="button">Majoitusvaraukset</a>
-        <a className="button">Asiakkaat</a>
-        <a className="button">Laskutus</a>
-        <a className="button">Raportit</a>
+    return (
+        <div className="menuBarBG">
+            <div className="buttons">
+                <a className="button">Alueet ja mökit</a>
+                <a className="button">Palvelut</a>
+                <a className="button">Majoitusvaraukset</a>
+                <a className="button">Asiakkaat</a>
+                <a className="button">Laskutus</a>
+                <a className="button">Raportit</a>
 
-        <div className="search-container">
-          {" "}
-          {/* Lisätty tämä divi hakuelementeille */}
-          <input type="text" className="search-input" placeholder="Etsi" />
-          <button type="submit" className="search-button">
-            Etsi
-          </button>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Etsi"
+                    />
+                    <button type="submit" className="search-button">
+                        Etsi
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
