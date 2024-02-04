8abdbb04458b6c98940d4bb17553f3946e7976db
Sauli <sauli97@gmail.com>
2024-02-04 17:57:42 +0200

    Region List added

--- Frontend/VillageBase_fe/src/components/MainCenterBox.tsx
+++ Frontend/VillageBase_fe/src/components/MainCenterBox.tsx
@@ -1,7 +1,14 @@
 import "./MainCenterBox.css";
+import RegionPage from "./RegionPage";
+
+
 
 function MainCenterBox() {
-    return <div className="mainbar"></div>;
+    return <div className="mainbar">
+        <div className="page">
+            <RegionPage />
+        </div>
+    </div>;
 }
 
 export default MainCenterBox;
---
+++ Frontend/VillageBase_fe/src/components/RegionPage.tsx
@@ -0,0 +1,37 @@
+import "./RegionPage.css";
+
+// Fukntion runko
+export default function RegionPage() {
+    return (
+
+        <div className="region-BG">
+            <div className="regionTitle">Alueet</div>
+
+            <div className="regionList">
+                <ul className="list">
+                    <li className="listItem">Alue1</li>
+                    <li className="listItem">Alue2</li>
+                    <li className="listItem">Alue3</li>
+                    <li className="listItem">Alue4</li>
+                    <li className="listItem">Alue5</li>
+                    <li className="listItem">Alue6</li>
+                    <li className="listItem">Alue7</li>
+                    <li className="listItem">Alue8</li>
+                    <li className="listItem">Alue9</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                    <li className="listItem">Alue10</li>
+                </ul>
+            </div>
+
+
+        </div>
+
+    );
+}
