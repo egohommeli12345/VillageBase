0adfadc886cc86fcae0e3afbd496d5d0c795df41
Tam Phuong <Tam.Phuong@edu.savonia.fi>
2024-02-07 15:21:27 +0200

    Add interface files for Billing, Post, Report, Reservation, and ReservationService

---
+++ Frontend/VillageBase_fe/src/components/ReservationComponents/ReservationInterface.ts
@@ -0,0 +1,10 @@
+export interface ReservationInterface {
+    // typed variables
+    varaus_id: number;
+    asiakas_id: number;
+    mokki_mokki_id: number;
+    varattu_pvm: string;
+    vahvistus_pvm: string;
+    varattu_alkupvm: string;
+    varattu_loppupvm: string;
+}
---
+++ Frontend/VillageBase_fe/src/components/ServiceComponents/ReservationServiceInterface.ts
@@ -0,0 +1,6 @@
+export interface ReservationServiceInterface {
+    // typed variables
+    varaus_id: number;
+    palvelu_id: number;
+    lkm: number;
+}
