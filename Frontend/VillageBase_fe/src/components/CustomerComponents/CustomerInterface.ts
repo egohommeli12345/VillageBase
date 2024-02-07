dc4155a800622653bf126a2f52405be16edb7c43
Tam Phuong <Tam.Phuong@edu.savonia.fi>
2024-02-07 14:45:02 +0200

    Add interface files for Cottage, Customer, and Service components

---
+++ Frontend/VillageBase_fe/src/components/CustomerComponents/CustomerInterface.ts
@@ -0,0 +1,10 @@
+export interface CustomerInterface {
+    // typed variables
+    asiakas_id: number;
+    postinro: string;
+    etunimi: string;
+    sukunimi: string;
+    lahiosoite: string;
+    email: string;
+    puhelinnro: string;
+}
---
+++ Frontend/VillageBase_fe/src/components/ServiceComponents/ServiceInterface.ts
@@ -0,0 +1,10 @@
+export interface ServiceInterface {
+    // typed variables
+    palvelu_id: number;
+    alue_id: number;
+    nimi: string;
+    tyyppi: number;
+    kuvaus: string;
+    hinta: number;
+    alv: number;
+}
