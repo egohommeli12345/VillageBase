4002eedb37540762c9c24396a3274dc278d317e1
egohommeli12345 <pirnes.samuli@gmail.com>
2024-02-07 14:42:03 +0200

    ADD - regions page maps data fetched from database

--- Frontend/VillageBase_fe/src/components/RegionComponents/RegionPage.tsx
+++ Frontend/VillageBase_fe/src/components/RegionComponents/RegionPage.tsx
@@ -1,36 +1,36 @@
 import styles from "./RegionPage.module.css";
+import { RegionInterface } from "./RegionInterface";
+import { useEffect, useState } from "react";
+import { RegionFetch } from "./RegionFetch";
 
 // Function for RegionPage
 export default function RegionPage() {
-    
+    // useState hook for mapping the regions to RegionInterface objects
+    const [regions, setRegions] = useState<RegionInterface[]>([]);
+
+    useEffect(() => {
+        RegionFetch().then((data) => {
+            setRegions(data);
+        });
+    }, []);
 
     return (
         <div className={styles.regionBG}>
             <div className={styles.regionTitle}>Alueet</div>
             <div className={styles.regionList}>
                 <ul className={styles.list}>
-                    <li className={styles.listItem}>Kuopio</li>
-                    <li className={styles.listItem}>Kuusamo</li>
-                    <li className={styles.listItem}>Kittilä</li>
-                    <li className={styles.listItem}>Äkäslompolo</li>
-                    <li className={styles.listItem}>Alue5</li>
-                    <li className={styles.listItem}>Alue6</li>
-                    <li className={styles.listItem}>Alue7</li>
-                    <li className={styles.listItem}>Alue8</li>
-                    <li className={styles.listItem}>Alue9</li>
-                    <li className={styles.listItem}>Alue10</li>
-                    <li className={styles.listItem}>Alue11</li>
-                    <li className={styles.listItem}>Alue12</li>
-                    <li className={styles.listItem}>Alue13</li>
-                    <li className={styles.listItem}>Alue14</li>
-                    <li className={styles.listItem}>Alue15</li>
-                    <li className={styles.listItem}>Alue16</li>
-                    <li className={styles.listItem}>Alue17</li>
-                    <li className={styles.listItem}>Alue18</li>
-                    <li className={styles.listItem}>Alue19</li>
-                    <li className={styles.listItem}>Alue20</li>
-                    <li className={styles.listItem}>Alue21</li>
-                    <li className={styles.listItem}>Alue22</li>
+                    {regions.map((region) => (
+                        <li className={styles.listItem} key={region.alue_id}>
+                            <div className={styles.regionId}>
+                                Alueen ID:
+                                {region.alue_id}
+                            </div>
+                            <div className={styles.regionName}>
+                                Alueen nimi:
+                                {region.nimi}
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
