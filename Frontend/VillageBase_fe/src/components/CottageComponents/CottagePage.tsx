03998b15f30e7c4db1ea4dac2a9ee9075a6c9111
egohommeli12345 <pirnes.samuli@gmail.com>
2024-02-08 13:47:12 +0200

    Refactor CottagePage component

--- Frontend/VillageBase_fe/src/components/CottageComponents/CottagePage.tsx
+++ Frontend/VillageBase_fe/src/components/CottageComponents/CottagePage.tsx
@@ -5,53 +5,77 @@ import { CottageInterface } from "./CottageInterface";
 
 // Function for CottagePage
 export default function CottagePage() {
-  // useState hook for mapping the cottages to CottageInterface objects
-  const [cottages, setCottages] = useState<CottageInterface[]>([]);
-  const [sortedCottages, setSortedCottages] = useState<CottageInterface[]>([]);
+    // useState hook for mapping the cottages to CottageInterface objects
+    const [cottages, setCottages] = useState<CottageInterface[]>([]);
+    const [sortedCottages, setSortedCottages] = useState<CottageInterface[]>(
+        []
+    );
 
-  // State to track the active container
-  const [activeContainerId, setActiveContainerId] = useState<number | null>(null);
+    // State to track the active container
+    const [activeContainerId, setActiveContainerId] = useState<number | null>(
+        null
+    );
 
-  // Function to toggle the active container
-  const makeActive = (id: number) => {
-    setActiveContainerId(id);
-  };
+    // Function to toggle the active container
+    const makeActive = (id: number) => {
+        if (id === activeContainerId) {
+            setActiveContainerId(null);
+            return;
+        }
+        setActiveContainerId(id);
+    };
 
-  useEffect(() => {
-    CottageFetch().then((data) => {
-      setCottages(data);
-      setSortedCottages(data.toSorted(CompareCottages));
-    });
-  }, []);
+    useEffect(() => {
+        CottageFetch().then((data) => {
+            setCottages(data);
+            setSortedCottages(data.toSorted(CompareCottages));
+        });
+    }, []);
 
-  return (
-    <div className={styles.cottageBG}>
-      <div className={styles.cottageTitle}>Mökit</div>
-      <div className={styles.cottageCardsContainer}>
-        {sortedCottages.map((cottage) => (
-          <div
-            className={`${styles.card} ${activeContainerId === cottage.mokki_id ? styles.active : ''}`}
-            key={cottage.mokki_id}
-            onClick={() => makeActive(cottage.mokki_id)}
-          >
-            <div className={styles.cardHeader}>
-              {cottage.mokkinimi} {cottage.mokki_id}
+    return (
+        <div className={styles.cottageBG}>
+            <div className={styles.cottageTitle}>Mökit</div>
+            <div className={styles.cottageCardsContainer}>
+                {sortedCottages.map((cottage) => (
+                    <div
+                        className={`${styles.card} ${
+                            activeContainerId === cottage.mokki_id
+                                ? styles.active
+                                : ""
+                        }`}
+                        key={cottage.mokki_id}
+                        onClick={() => makeActive(cottage.mokki_id)}
+                    >
+                        <div className={styles.cardHeader}>
+                            {cottage.mokkinimi} {cottage.mokki_id}
+                        </div>
+                        <div className={styles.cardBody}>
+                            <p>
+                                <strong>Osoite:</strong> {cottage.katuosoite},{" "}
+                                {cottage.postinro}
+                            </p>
+                            <p>
+                                <strong>Hinta:</strong> {cottage.hinta}€/yö
+                            </p>
+                            <p>
+                                <strong>Kuvaus:</strong> {cottage.kuvaus}
+                            </p>
+                            <p>
+                                <strong>Henkilömäärä:</strong>{" "}
+                                {cottage.henkilomaara} henkilöä
+                            </p>
+                            <p>
+                                <strong>Varustelu:</strong> {cottage.varustelu}
+                            </p>
+                        </div>
+                        {/* <button className={styles.cardButton}>Lisätietoja</button> */}
+                    </div>
+                ))}
             </div>
-            <div className={styles.cardBody}>
-              <p><strong>Osoite:</strong> {cottage.katuosoite}, {cottage.postinro}</p>
-              <p><strong>Hinta:</strong> {cottage.hinta}€/yö</p>
-              <p><strong>Kuvaus:</strong> {cottage.kuvaus}</p>
-              <p><strong>Henkilömäärä:</strong> {cottage.henkilomaara} henkilöä</p>
-              <p><strong>Varustelu:</strong> {cottage.varustelu}</p>
-            </div>
-            {/* <button className={styles.cardButton}>Lisätietoja</button> */}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 function CompareCottages(a: CottageInterface, b: CottageInterface) {
-  return a.mokki_id - b.mokki_id;
+    return a.mokki_id - b.mokki_id;
 }
