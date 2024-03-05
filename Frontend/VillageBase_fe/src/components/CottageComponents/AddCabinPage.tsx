4fdb2e57d9b69c2fca3ba9cca19b0e531c5478c7
Sauli <sauli97@gmail.com>
2024-03-05 02:15:49 +0200

    Update AddCabinPage styles and form layout

--- Frontend/VillageBase_fe/src/components/CottageComponents/AddCabinPage.tsx
+++ Frontend/VillageBase_fe/src/components/CottageComponents/AddCabinPage.tsx
@@ -17,43 +17,46 @@ const AddCabinPage = () => {
 
     return (
         <div className={styles.cabinBR}>
-            <div>Lisää uusi mökki</div>
+            <div className={styles.title}>Lisää uusi mökki</div>
             <form className={styles.form} onSubmit={handleSubmit}>
-                <div className={styles.contentDiv}>
-                    <label htmlFor="cabinName">Mökin nimi:</label>
-                    <input
-                        id="cabinName"
-                        type="text"
-                        value={cabinName}
-                        onChange={(e) => setCabinName(e.target.value)}
-                    />
-                </div>
-                <div className={styles.contentDiv}>
-                    <label htmlFor="location">Sijainti:</label>
-                    <input
-                        id="location"
-                        type="text"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                    />
-                </div>
-                <div className={styles.contentDiv}>
-                    <label htmlFor="description">Kuvaus:</label>
+                {/* <div className={styles.flexDiv}> */}
+                    <label>
+                        <input
+                            id="cabinName"
+                            type="text"
+                            value={cabinName}
+                            onChange={(e) => setCabinName(e.target.value)}
+                        />
+                        <span>Mökin nimi:</span>
+                    </label>
+                    <label>
+                        <input
+                            id="location"
+                            type="text"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                        <span>Sijainti:</span>
+                    </label>
+                {/* </div> */}
+                <label> 
                     <textarea
                         id="description"
+                        className={styles.descrip}
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                     />
-                </div>
-                <div className={styles.contentDiv}>
-                    <label htmlFor="price">Hinta:</label>
+                    <span>Kuvaus:</span>
+                </label>
+                <label>
                     <input
                         id="price"
                         type="text"
                         value={price}
                         onChange={(e) => setPrice(e.target.value)}
                     />
-                </div>
+                    <span>Hinta:</span>
+                </label>
                 {/* Tarvittaessa lisää kenttiä */}
                 <button type="submit">Lisää mökki</button>
             </form>
