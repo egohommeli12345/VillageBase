be464e0bbb6d4b25732e280be57ee8e9126a11dd
egohommeli12345 <pirnes.samuli@gmail.com>
2024-03-10 21:33:19 +0200

    ADD - GeneratePdfInvoice function file for billingpage

--- Frontend/VillageBase_fe/src/components/ReportComponents/ReportPage.tsx
+++ Frontend/VillageBase_fe/src/components/ReportComponents/ReportPage.tsx
@@ -9,111 +9,13 @@ import { useToolState } from "../MainComponents/ToolStateContext";
 function ReportPage() {
     const { setOnLandingPage } = useToolState();
 
-    const generatePDF = () => {
-        const doc = new jsPDF();
-
-        const pageWidth = doc.internal.pageSize.getWidth();
-        const pageHeight = doc.internal.pageSize.getHeight();
-
-        const rowHeight = 5;
-        const columnWidth = 5;
-        let x = 0;
-        let y = 0;
-
-        doc.setFontSize(28);
-        doc.setFont("Helvetica", "bold");
-
-        x = 25;
-        y = 25;
-        doc.text("VillageNewbies", x, y);
-        doc.setFont("Helvetica", "normal");
-        doc.text("LASKU", pageWidth - x, y, { align: "right" });
-        doc.setFontSize(12);
-        doc.text("# 1234", pageWidth - x, 33, { align: "right" });
-
-        y = 50;
-
-        doc.text(
-            ["Lähettäjä:", "VillageNewbies", "Kylätie 24, 12345", "Kylälä"],
-            x,
-            y
-        );
-        y = 80;
-        doc.text(
-            ["Saaja:", "Manu Maksaja", "Maksukatu 0€, 00000", "Köyhälä"],
-            x,
-            y
-        );
-
-        y = 75;
-        x = pageWidth - 70;
-        doc.text("Päivämäärä:", x, y, { align: "right" });
-        doc.text("01.01.2024", x + 5, y);
-        y += 10;
-        doc.text("Eräpäivä:", x, y, { align: "right" });
-        doc.text("01.02.2024", x + 5, y);
-        y += 10;
-        doc.text("Summa:", x, y, { align: "right" });
-        doc.text("884.00 €", x + 5, y);
-
-        y = 110;
-
-        doc.line(10, y, pageWidth - 10, y);
-        y += 10;
-        x = 25;
-
-        doc.text("Palvelut", x, y);
-        doc.text("Määrä", x * 4, y);
-        doc.text("Yksikköhinta", x * 5, y);
-        doc.text("Summa", pageWidth - x, y, { align: "right" });
-        y += 5;
-
-        doc.line(10, y, pageWidth - 10, y);
-        y += 10;
-
-        doc.text("Mökki:", x, y);
-        y += 5;
-        doc.text("Lakeside Haven", x, y);
-        y += 5;
-        doc.text("2024.06.01 14.00 - 2024.06.08 10.00", x, y);
-        doc.text("8", x * 4, y);
-        doc.text("95.50 €", x * 5, y);
-        doc.text("764.00 €", pageWidth - x, y, { align: "right" });
-        y += 10;
-        doc.text("Lisäpalvelu:", x, y);
-        y += 5;
-        doc.text("Lake Excursions", x, y);
-        doc.text("3", x * 4, y);
-        doc.text("40.00 €", x * 5, y);
-        doc.text("120.00 €", pageWidth - x, y, { align: "right" });
-        y += 10;
-
-        doc.line(10, y, pageWidth - 10, y);
-        y += 10;
-
-        doc.text("884.00 €", pageWidth - x, y, { align: "right" });
-
-        y += 10;
-
-        doc.line(10, y, pageWidth - 10, y);
-
-        doc.save("vb_invoice_template.pdf");
-    };
-
-    const debug = () => {
-        const debug = new jsPDF();
-        debug.text("Hello world!", 25, 25);
-        console.log(debug.internal.pageSize.getHeight());
-    };
-
     useEffect(() => {
         setOnLandingPage(true);
     }, []);
 
     return (
-        <div className={styles.reportBg}>
-            <button onClick={generatePDF}>Generate PDF</button>
-            <button onClick={debug}>debug</button>
+        <div className={styles.reportBG}>
+            <div>KONTSAA</div>
         </div>
     );
 }
