af632d26f9aecaa807230c26175c8987f161fe4a
Tam Phuong <Tam.Phuong@edu.savonia.fi>
2024-04-19 11:05:12 +0300

    Fix styling issue in MenuBar component

--- Frontend/VillageBase_fe/src/components/MainComponents/MenuBar.tsx
+++ Frontend/VillageBase_fe/src/components/MainComponents/MenuBar.tsx
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./MenuBar.module.css";
 import ToolBar from "./ToolBar";
 import { useSearch } from "./SearchContext";
 import { useSortType } from "../SortingComponents/SortTypeContext.tsx";
 export default function MenuBar() {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const { setSearchQuery } = useSearch();
     const { setSortBy, setSortType, sortBy } = useSortType();
@@ -51,29 +52,34 @@ export default function MenuBar() {
         navigate("/raportit");
     };
 
+    const getActiveClass = (path: string) => {
+        // Vertaillaan, että sivuston polku alkaa annetulla polulla
+        return location.pathname.startsWith(path) ? styles.active : '';
+    }
+    
     return (
         <div className={styles.menuBar}>
             <div className={styles.menuBarBG}>
-                <div className={styles.buttons}>
-                    <div className={styles.btn} onClick={goToRegionsPage}>
+            <div className={styles.buttons}>
+                    <div className={`${styles.btn} ${getActiveClass('/alueet')}`} onClick={goToRegionsPage}>
                         Alueet
                     </div>
-                    <div className={styles.btn} onClick={goToCottagesPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/mokit')}`} onClick={goToCottagesPage}>
                         Mökit
                     </div>
-                    <div className={styles.btn} onClick={goToServicesPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/palvelut')}`} onClick={goToServicesPage}>
                         Palvelut
                     </div>
-                    <div className={styles.btn} onClick={goToReservationsPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/varaukset')}`} onClick={goToReservationsPage}>
                         Varaukset
                     </div>
-                    <div className={styles.btn} onClick={goToCustomersPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/asiakkaat')}`} onClick={goToCustomersPage}>
                         Asiakkaat
                     </div>
-                    <div className={styles.btn} onClick={goToBillingPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/laskutus')}`} onClick={goToBillingPage}>
                         Laskutus
                     </div>
-                    <div className={styles.btn} onClick={goToReportsPage}>
+                    <div className={`${styles.btn} ${getActiveClass('/raportit')}`} onClick={goToReportsPage}>
                         Raportit
                     </div>
                 </div>
